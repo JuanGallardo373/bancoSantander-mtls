d9e1357980090c2f5a0bb9ee87bc94e98f049095
Juan <juanigallardo152@gmail.com>
2026-06-11 00:45:02 -0300

    Remove duplicate echo statement in generate-cert.sh for clarity

--- cliente-bancobbva/generate-cert.sh
+++ cliente-bancobbva/generate-cert.sh
@@ -6,4 +6,3 @@ openssl genpkey -algorithm RSA -out certs/bbva-key.pem
 echo "Generando la solicitud de firma (CSR)..."
 openssl req -new -key certs/bbva-key.pem -out certs/bbva-req.pem -subj "/C=AR/ST=Buenos Aires/L=CABA/O=BBVA/CN=BancoBBVA"
 echo "CSR del banco BBVA creada"
-
