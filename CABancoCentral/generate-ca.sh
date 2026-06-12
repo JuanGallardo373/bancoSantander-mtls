4b156f0b187d6549e385c7055be69fe49dc8b753
Juan Gallardo <juanigallardo152@gmail.com>
2026-06-12 00:32:52 -0300

    Update generate-ca.sh

--- CABancoCentral/generate-ca.sh
+++ CABancoCentral/generate-ca.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 mkdir -p private newcerts
+touch index.txt
+echo 1000 > serial
 echo "Generando clave privada y certificado del Banco Central (CA)..."
 openssl req -new -x509 -days 3650 -extensions v3_ca -keyout private/cakey.pem \
 	-out cacert.pem -config ca.conf -subj "/C=AR/ST=Buenos Aires/L=CABA/CN=BancoCentralCA" 
