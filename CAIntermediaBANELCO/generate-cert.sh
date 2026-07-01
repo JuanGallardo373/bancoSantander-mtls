09f24825a4d08603498e46227bfb64551b706213
Juan <juanigallardo152@gmail.com>
2026-06-30 21:55:49 -0300

    Update Banelco

--- CAIntermediaBANELCO/generate-cert.sh
+++ CAIntermediaBANELCO/generate-cert.sh
@@ -5,7 +5,7 @@ touch index.txt
 echo 1000 > serial
 echo "Generando clave privada y CSR de BANELCO (CA Intermedia)..."
 openssl req -new -nodes -newkey rsa:2048 \
-    -keyout certs/banelco-inter.key -out certs/banelco-inter.csr \
+    -keyout banelco-inter.key -out banelco-inter.csr \
     -config ../CABancoCentral/ca.conf \
     -subj "/C=AR/ST=Buenos Aires/L=CABA/O=BANELCO SA/CN=BanelcoCAIntermedia"
 echo "Solicitud de firma creada correctamente"
